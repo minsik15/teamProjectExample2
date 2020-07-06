40907305b08ad535c6730eeafa9f63c8dadf8c7b
minsik <minsik15@naver.com>
2020-07-06 17:17:10 +0900

    project init comit

---
+++ src/main/java/tommy/spring/SpringTeam/App.java
@@ -0,0 +1,13 @@
+package tommy.spring.SpringTeam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
